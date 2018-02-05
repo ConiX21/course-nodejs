5314b917429e10726e7e107872f9ded6b5d7d5f9
ConiX21 <nicolas.gasquet@nerys.biz>
2018-02-05 13:18:48 +0100

    Add REPL

---
+++ REPL/index.js
@@ -0,0 +1,26 @@
+//RELP
+// Read, Eval, Print, Loop
+
+/*
+ run node
+- Read the command : 
+    Example : 
+        3 + 3
+- Execute(Eval) command
+- Print result
+    Example : 
+        6
+- Wait another command (Loop)
+ You can never use console.log
+
+ - You can Press tab-tab for watching globals objects
+ - same for command
+    global. (tab-twice)
+- You have completion with tab touch
+- try :
+    var arr = [];
+    arr. (tab twice)
+
+- You can use _ to capture last output
+- You can use .help to know command RELP
+*/
